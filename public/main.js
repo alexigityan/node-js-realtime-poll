59d4b63694c88ddaf7c073957e60e5579d267ed2
alexigityan <alexigityan@yahoo.com>
2019-06-16 15:56:31 +0400

    fixed a bug in client javascript

--- public/main.js
+++ public/main.js
@@ -20,61 +20,70 @@ form.addEventListener('submit', event => {
   }
 })
 
-fetch(`${TARGET_URL}/poll`)
-  .then( res => res.json() )
-  .then( votes => {
-    const voteCount = votes.length
-    const voteValues = votes.reduce((values, data)=>{
-      if(values.hasOwnProperty(data.vote)) {
-        values[data.vote] += data.points
-      } else {
-        values[data.vote] = data.points
+let voteCount = 0
+
+let dataPoints = [
+  { label: 'Windows', y: 0 },
+  { label: 'Linux', y: 0 },
+  { label: 'MacOS', y: 0 }
+]
+
+const chartContainer = document.getElementById('chart-container')
+
+if(chartContainer) {
+  const chart = new CanvasJS.Chart('chart-container', {
+    animationEnabled: true,
+    theme: 'theme1',
+    title: {
+      text: `OS Results based on ${voteCount} votes`
+    },
+    data: [
+      {
+        type: 'column',
+        dataPoints: dataPoints
       }
-      return values
-    }, {})
-
-    let dataPoints = [
-      { label: 'Windows', y: voteValues.Windows },
-      { label: 'Linux', y: voteValues.Linux },
-      { label: 'MacOS', y: voteValues.MacOS}
     ]
+  })
+  chart.render()
 
-    const chartContainer = document.getElementById('chart-container')
+  const pusher = new Pusher('ab26edbeadaf4d0cd876', {
+    cluster: 'eu',
+    forceTLS: true
+  })
+  
+  const channel = pusher.subscribe('poll-channel')
+  channel.bind('poll-vote-event', function(data) {
+    voteCount += 1
+    chart.set('title', { text: `OS Results based on ${voteCount} votes` } )
+    dataPoints = dataPoints.map( point => {
+      if (point.label === data.vote) {
+        point.y += data.points
+      }
+      return point
+    })
+    chart.render()
+  })
 
-    if(chartContainer) {
-      const chart = new CanvasJS.Chart('chart-container', {
-        animationEnabled: true,
-        theme: 'theme1',
-        title: {
-          text: `OS Results based on ${voteCount} votes`
-        },
-        data: [
-          {
-            type: 'column',
-            dataPoints
-          }
-        ]
+  fetch(`${TARGET_URL}/poll`)
+    .then( res => res.json() )
+    .then( votes => {
+      voteCount = votes.length
+      const voteValues = votes.reduce((values, data)=>{
+        if(values.hasOwnProperty(data.vote)) {
+          values[data.vote] += data.points
+        } else {
+          values[data.vote] = data.points
+        }
+        return values
+      }, {})
+      dataPoints = dataPoints.map( point => {
+        point.y = voteValues[point.label]
+        return point
       })
       chart.render()
-    
-      const pusher = new Pusher('ab26edbeadaf4d0cd876', {
-        cluster: 'eu',
-        forceTLS: true
-      })
-      
-      const channel = pusher.subscribe('poll-channel');
-      channel.bind('poll-vote-event', function(data) {
-        dataPoints = dataPoints.map( point => {
-          if (point.label === data.vote) {
-            point.y += data.points
-          }
-          return point
-        })
-        chart.render()
-      })
-    }
-  })
-  .catch( err => console.log(err))
+    })
+    .catch( err => console.log(err))
+}
 
 
 
